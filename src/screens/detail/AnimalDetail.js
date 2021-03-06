180e6f4dad6c44df275c593c49631c1c7ecc3425
D00198870 <widjajarenzo1@gmail.com>
2021-03-06 19:02:50 +0700

    autofocus fix

--- src/screens/detail/AnimalDetail.js
+++ src/screens/detail/AnimalDetail.js
@@ -187,7 +187,7 @@ export default function AnimalDetail ({ navigation, route }) {
         <Provider>
             <Portal>
                 <Modal visible={visible} onDismiss={hideModal} contentContainerStyle={containerStyle}>
-                    <TextInput style={{fontSize: 18, fontFamily: 'Sora-SemiBold', color: 'white', bottom: 50, textAlign: 'center'}}>
+                    <TextInput style={{fontSize: 18, fontFamily: 'Sora-SemiBold', color: 'white', bottom: 50, textAlign: 'center'}} autoFocus={true}>
                         This is a note
                     </TextInput>
                     <Button icon="check" mode="contained" color='#91CCFE' style={{top: 70, marginHorizontal: SPACING,}} onPress={hideModal} labelStyle={{fontFamily: 'Sora-SemiBold', fontSize: 15}}>

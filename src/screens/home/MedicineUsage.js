9ed739ca411f15c4fbbc3af5d1c48a1c12882672
D00198870 <widjajarenzo1@gmail.com>
2021-04-09 23:57:33 +0700

    feat: use query

--- src/screens/home/MedicineUsage.js
+++ src/screens/home/MedicineUsage.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import faker from 'faker';
 
 // COMPONENT
 import { View, FlatList, SafeAreaView } from 'react-native';
@@ -6,13 +7,30 @@ import { Button } from 'react-native-paper';
 import { PageHeader } from '../../components/atoms/PageHeader'
 import { MedicineUsageItemView } from '../../components/atoms/MedicineUsageItemView';
 
-// DATA
-import { MedicineUsageData } from '../../config/data/Medicine';
+// LOADER
+import { PageLoader } from '../../components/atoms/PageLoader';
+
+// QUERY
+import { useQuery } from '@apollo/client';
+import { GET_MEDICATION_USAGE_LIST } from '../../config/graphql/queries';
 
 // THEME
 import { SPACING, defaultBackground } from '../../config/theme';
 
 export default function MedicationUsage ({navigation}) {
+
+    // MEDICINE USAGE LIST
+    const { data, loading } = useQuery(GET_MEDICATION_USAGE_LIST);
+
+    if (loading) {
+        return <PageLoader />
+    }
+
+    const MedicineUsageData = data.administeredMedications.administeredMedications.map((item, index) => ({
+        ...item,
+        key: faker.random.uuid() 
+    }))
+
     return (
         <>
         <SafeAreaView style={{backgroundColor: defaultBackground}}>

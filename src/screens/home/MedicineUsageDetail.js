96a01abc179524ad6548b7d0ba0266eaab25fc6f
D00198870 <widjajarenzo1@gmail.com>
2021-04-15 16:00:45 +0700

    feat: add new query for animal tag

--- src/screens/home/MedicineUsageDetail.js
+++ src/screens/home/MedicineUsageDetail.js
@@ -1,120 +1,146 @@
-import * as React from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import * as React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 // COMPONENTS
-import { StyleSheet, View, Text, KeyboardAvoidingView } from 'react-native';
-import { PageHeader } from '../../components/atoms/PageHeader';
-import { Button } from 'react-native-paper';
+import { StyleSheet, View, Text, KeyboardAvoidingView } from "react-native";
+import { PageHeader } from "../../components/atoms/PageHeader";
+import { Button } from "react-native-paper";
 
 // THEME
-import { SPACING, width, height, defaultBackground, cardBackground, topOS } from '../../config/theme';
-import { ScrollView } from 'react-native';
+import {
+  SPACING,
+  width,
+  height,
+  defaultBackground,
+  cardBackground,
+  topOS,
+} from "../../config/theme";
+import { ScrollView } from "react-native";
 
 // SIZING
 export const CELL_HEIGHT = height * 0.18;
 
 const styles = StyleSheet.create({
-    header_inner: {
-        flex:1,
-        overflow: 'hidden',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        position: 'relative',
-        marginTop: topOS
-    },
-    name: {
-        fontSize: 25,
-        fontFamily: 'Sora-Bold',
-        color: '#F3F4B8',
-        paddingTop: SPACING,
-    },
-    medicineType: {
-        fontSize: 18,
-        top: 5,
-        opacity: 0.8,
-        color: 'white',
-        fontFamily: 'Sora-SemiBold',
-    },
-    medicineLabel: {
-        fontSize: 18, 
-        paddingTop: 35, 
-        color: 'white', 
-        opacity: 0.8, 
-        fontFamily: 'Sora-SemiBold',
-    },
-    medicineDesc: {
-        color: 'white', 
-        fontSize: 18, 
-        fontFamily: 'Sora-Bold',
-        top: 5,
-    },
-    border: {
-        borderBottomColor: '#9D9D9D', 
-        borderBottomWidth: 1, 
-        top: 20,
-    },
+  header_inner: {
+    flex: 1,
+    overflow: "hidden",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    position: "relative",
+    marginTop: topOS,
+  },
+  name: {
+    fontSize: 25,
+    fontFamily: "Sora-Bold",
+    color: "#F3F4B8",
+    paddingTop: SPACING,
+  },
+  medicineType: {
+    fontSize: 18,
+    top: 5,
+    opacity: 0.8,
+    color: "white",
+    fontFamily: "Sora-SemiBold",
+  },
+  medicineLabel: {
+    fontSize: 18,
+    paddingTop: 35,
+    color: "white",
+    opacity: 0.8,
+    fontFamily: "Sora-SemiBold",
+  },
+  medicineDesc: {
+    color: "white",
+    fontSize: 18,
+    fontFamily: "Sora-Bold",
+    top: 5,
+  },
+  border: {
+    borderBottomColor: "#9D9D9D",
+    borderBottomWidth: 1,
+    top: 20,
+  },
 });
 
-export default function MedicineUsageDetail ({ navigation, route }) {
+export default function MedicineUsageDetail({ navigation, route }) {
+  const {
+    administeredBy,
+    dateAdministered,
+    quantityAdministered,
+    quantityType,
+    reason,
+    animalTagNumber,
+    medicineName,
+  } = route.params;
 
-    // Object {
-    //     "__typename": "AdministeredMedication",
-    //     "administered_by": "Conor",
-    //     "animal_id": "5fc543e9e04ba94a7cdf94a5",
-    //     "date_of_administration": "2020-12-12T00:00:00.000Z",
-    //     "id": "6046436af1d177186403ef61",
-    //     "medication_id": "5fd89a8e915d5d3cb0015929",
-    //     "quantity_administered": 10,
-    //     "quantity_type": "MG",
-    //     "reason_for_administration": "as",
-    // }
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: defaultBackground }}>
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          paddingHorizontal: SPACING,
+          marginBottom: SPACING,
+        }}
+      >
+        <View style={styles.header_inner}>
+          <PageHeader
+            label="Medication Details"
+            goBack={navigation.goBack}
+            showChevron="true"
+          />
+        </View>
+      </View>
 
-    // Missing: withdrawal period (active/no active)
-    // Missing: Dose given
+      {/* CONFIRMATION CARD */}
+      <ScrollView style={{ paddingHorizontal: SPACING }}>
+        <View style={{ flex: 1, padding: SPACING, height: 500, top: SPACING }}>
+          <View
+            style={[
+              StyleSheet.absoluteFillObject,
+              { backgroundColor: cardBackground, borderRadius: 15 },
+            ]}
+          />
 
-    const {administeredBy, dateAdministered, quantityAdministered, quantityType, reason} = route.params;
+          <Text style={styles.name}>{medicineName}</Text>
+          <Text style={styles.medicineType}>DOSE</Text>
 
-    return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: defaultBackground }}>
-            <View style={{display: 'flex', flexDirection: 'row', alignItems: 'center', paddingHorizontal: SPACING, marginBottom: SPACING}}> 
-                <View style={styles.header_inner}>
-                    <PageHeader label="Medication Details" goBack={navigation.goBack} showChevron='true' />
-                </View>              
-            </View>   
+          <View style={styles.border} />
 
-                {/* CONFIRMATION CARD */}
-                <ScrollView style={{ paddingHorizontal: SPACING }}>
-                    <View style={{ flex: 1, padding: SPACING, height: 500, top: SPACING}}>
-                        <View style={[StyleSheet.absoluteFillObject, {backgroundColor: cardBackground, borderRadius: 15}]}/>
+          <View style={{ flexDirection: "row" }}>
+            <View>
+              <Text style={styles.medicineLabel}>Animal ID</Text>
+              <Text style={[styles.medicineDesc, { color: "#F3F4B8" }]}>
+                {animalTagNumber}
+              </Text>
+            </View>
+            <View style={{ position: "absolute", right: 0 }}>
+              <Text style={styles.medicineLabel}>Administered By</Text>
+              <Text
+                style={[
+                  styles.medicineDesc,
+                  { position: "absolute", right: 0, top: 60 },
+                ]}
+              >
+                {administeredBy}
+              </Text>
+            </View>
+          </View>
 
-                        <Text style={styles.name}>MISSING</Text>
-                        <Text style={styles.medicineType}>Medicament</Text>
+          <Text style={styles.medicineLabel}>Date of Administration</Text>
+          <Text style={styles.medicineDesc}>{dateAdministered}</Text>
 
-                        <View style={styles.border}/>
+          <Text style={styles.medicineLabel}>Quantity Administered</Text>
+          <Text style={styles.medicineDesc}>
+            {quantityAdministered} {quantityType}
+          </Text>
 
-                        <View style={{flexDirection: 'row'}}>
-                            <View>
-                                <Text style={styles.medicineLabel}>Animal ID</Text>
-                                <Text style={[styles.medicineDesc, {color: '#F3F4B8'}]}>MISSING</Text>
-                            </View>
-                            <View style={{position: 'absolute', right: 0}}>
-                                <Text style={styles.medicineLabel}>Administered By</Text>
-                                <Text style={[styles.medicineDesc, {position: 'absolute', right: 0, top: 60,}]}>{administeredBy}</Text>
-                            </View>
-                        </View>
-
-                        <Text style={styles.medicineLabel}>Date of Administration</Text>
-                        <Text style={styles.medicineDesc}>{dateAdministered}</Text>
-
-                        <Text style={styles.medicineLabel}>Quantity Administered</Text>
-                        <Text style={styles.medicineDesc}>{quantityAdministered} {quantityType}</Text>
-
-                        <Text style={styles.medicineLabel}>Reason</Text>
-                        <Text style={styles.medicineDesc}>{reason}</Text>
-                    </View>
-                </ScrollView>
-                
-        </SafeAreaView>
-    )
+          <Text style={styles.medicineLabel}>Reason</Text>
+          <Text style={styles.medicineDesc}>{reason}</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
 }

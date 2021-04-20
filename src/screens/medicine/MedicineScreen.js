89d58971017e36dc318064616623234307fcae4d
D00198870 <widjajarenzo1@gmail.com>
2021-04-20 14:46:20 +0700

    refactor: move search bar as individual component

--- src/screens/medicine/MedicineScreen.js
+++ src/screens/medicine/MedicineScreen.js
@@ -1,14 +1,18 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { useIsFocused, useNavigation } from '@react-navigation/core';
 import { useScrollToTop } from '@react-navigation/native';
-import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons'; 
+import { MaterialCommunityIcons } from '@expo/vector-icons'; 
 
 // COMPONENTS
-import { View, FlatList, SafeAreaView, StyleSheet, TouchableHighlight, Text, RefreshControl } from 'react-native';
+import {
+    View,
+    FlatList,
+    SafeAreaView,
+    StyleSheet,
+} from "react-native";
 import { Button } from 'react-native-paper';
 import { PageHeader } from '../../components/atoms/PageHeader'
 import { MedicineItemView } from '../../components/atoms/MedicineItemView';
-import { SearchBar } from 'react-native-elements';
+import SearchBarList from '../../components/molecules/SearchBar';
 
 // LOADER
 import { PageLoader } from '../../components/atoms/PageLoader';
@@ -16,12 +20,15 @@ import { PageLoader } from '../../components/atoms/PageLoader';
 // QUERY
 import { useQuery } from '@apollo/client';
 import { GET_MEDICATIONS } from '../../config/graphql/queries';
- 
-// ANIMATION
-import Animated, { EasingNode } from 'react-native-reanimated'
- 
+
 // THEME
-import {  width, height, SPACING, defaultBackground, cardBackground, medicineLevelLow, medicineLevelMedium, medicineLevelHigh, CELL_HEIGHT, topOS } from '../../config/theme';
+import {
+    SPACING,
+    defaultBackground,
+    cardBackground,
+    width,
+    topOS,
+} from "../../config/theme";
 
 const styles = StyleSheet.create({
     header_inner: {
@@ -33,102 +40,13 @@ const styles = StyleSheet.create({
       position: 'relative',
       marginTop: topOS
     },
-    search_icon_box: {
-      width: 44,
-      height: 44,
-      borderRadius: 15, 
-      backgroundColor: '#E4E5E9',
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginLeft: 'auto',
-    },
-    input_box: {
-      height: 50,
-      flexDirection: 'row',
-      alignItems: 'center',
-      position: 'absolute',
-      width: width - 20,
-    },
-    back_icon_box: {
-      width: 50,
-      height: 50,
-      borderRadius: 40,
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginRight: 5,
-      backgroundColor: defaultBackground
-    },
-    input: {
-      backgroundColor: defaultBackground, 
-      borderTopWidth: 0, 
-      borderBottomWidth: 0, 
-      width: width - 60, 
-      position: 'absolute', 
-      right: 0,
-    },
 })
 
 export default function MedicineScreen ({navigation}, props) {
     const ref = React.useRef(null);
     useScrollToTop(ref);
 
-    // ANIMATION
-    const { Value, timing } = Animated
-    const [isFocused, setIsFocused] = useState(true);
-    const ref_input = useRef();
-
-    const input_box_translate_x = useRef(new Value(width)).current;
-    const back_button_opacity = useRef(new Value(0)).current;
-
-    // SEARCH
-    const [searchText, setSearchText] = useState('');
     const [filteredData, setFilteredData] = useState([]);
-
-    const onFocus = () => {
-        setIsFocused(true);
-
-        const input_box_translate_x_config = {
-            duration: 200,
-            toValue: 0,
-            easing: EasingNode.inOut(EasingNode.ease)
-        }
-
-        const back_button_opacity_config = {
-            duration: 200,
-            toValue: 1,
-            easing: EasingNode.inOut(EasingNode.ease)
-        }
-
-        // RUN ANIMATION
-        timing(input_box_translate_x, input_box_translate_x_config).start();
-        timing(back_button_opacity, back_button_opacity_config).start();
-
-        ref_input.current.focus();
-    }
-
-    const onBlur = () => {
-        setIsFocused(true);
-
-        const input_box_translate_x_config = {
-            duration: 200,
-            toValue: width,
-            easing: EasingNode.inOut(EasingNode.ease)
-        }
-
-        const back_button_opacity_config = {
-            duration: 50,
-            toValue: 0,
-            easing: EasingNode.inOut(EasingNode.ease)
-        }
-
-        // RUN ANIMATION
-        timing(input_box_translate_x, input_box_translate_x_config).start();
-        timing(back_button_opacity, back_button_opacity_config).start();
-
-        ref_input.current.blur();
-    }
 
     const { data, loading, refetch } = useQuery(GET_MEDICATIONS);
 
@@ -147,67 +65,13 @@ export default function MedicineScreen ({navigation}, props) {
 
     const MedicineList = data.medications.medications;
 
-    const search = (searchText) => {
-        setSearchText(searchText);
-
-        let filteredData = MedicineList.filter(function (item) {
-            const medicine_name_search = item.medication_name.includes(searchText);
-            const medicine_type = item.medicine_type.includes(
-              searchText.toUpperCase()
-            );
-            
-            return medicine_name_search || medicine_type;
-        });
-
-        setFilteredData(filteredData);
-    }
-
     return (
         <>
             <SafeAreaView style={{backgroundColor: defaultBackground,}}>
                 <View style={{display: 'flex', flexDirection: 'row', alignItems: 'center', paddingHorizontal: SPACING, marginBottom: SPACING}}> 
                     <View style={styles.header_inner}>
-
                         <PageHeader label="My Medicine" goBack={navigation.goBack} showChevron='false' />
-
-                        <TouchableHighlight 
-                            activeOpacity={1}
-                            underlayColor={"#ccd0d5"}
-                            onPress={onFocus}
-                            style={styles.search_icon_box}
-                        >
-                            <MaterialIcons name="search" size={30} color={cardBackground} />
-                        </TouchableHighlight>
-
-                        <Animated.View
-                            style={[ styles.input_box, {transform: [{translateX: input_box_translate_x}] } ]}
-                        >
-                            <Animated.View style={{opacity: back_button_opacity}}>
-                                <TouchableHighlight
-                                    activeOpacity={1}
-                                    underlayColor={"#ccd0d5"}
-                                    onPress={onBlur}
-                                    style={styles.back_icon_box}
-                                >
-                                    <MaterialIcons name="arrow-back-ios" size={30} color="white" />
-                                </TouchableHighlight>
-                            </Animated.View>
-                            <SearchBar 
-                                containerStyle={styles.input}
-                                inputContainerStyle={{backgroundColor: '#E4E5E9',}}
-                                inputStyle={{color: cardBackground, fontFamily: 'Sora-SemiBold'}}
-                                round
-                                searchIcon={null}
-                                placeholder='Search Medicine Name'
-                                returnKeyType='done' 
-                                clearIcon={true}
-                                ref={ref_input}
-                                value={searchText}
-                                onChangeText={search}
-                                onClear={onBlur}
-                                onSubmitEditing={onBlur}
-                            />
-                        </Animated.View>
+                        <SearchBarList items={MedicineList} setFilteredData={setFilteredData} fromScreen='medicine'/>
                     </View>              
                 </View>        
             </SafeAreaView>
